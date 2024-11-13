1ddb1335196f202d55ee0dce193a54dcf89f233e
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-11-13 16:35:56 -0500

    ANOVA and correlation tests

--- pages/Parameter Correlation Analysis.py
+++ pages/Parameter Correlation Analysis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -9,25 +10,50 @@ wq['Quality'] = wq['Water Quality'].map({0: 'Execellent', 1: 'Good', 2: 'Poor'})
 
 parameters = wq.columns[:-2]
 
+# Normality test
+# DataFrame to store Shapiro-Wilk results
+shapiro_results = pd.DataFrame(columns=['P-Value', 'Normally Distributed'])
 
-correlation_matrix = wq[parameters].corr()
+for column in parameters:
+    stat, p_value = stats.shapiro(wq[column])
 
-st.header("Parameter Correlation Heatmap")
+    shapiro_results.loc[column] = [str(p_value), int(p_value < .05)]
+
+# To show table results on page
+st.subheader("Shapiro-Wilk Results for Each Parameter")
+st.dataframe(shapiro_results)
+
+
+correlation_matrix = pd.DataFrame()
+p_value_matrix = pd.DataFrame()
+
+for col1 in parameters:
+    for col2 in parameters:
+        corr, p_value = stats.pearsonr(wq[col1], wq[col2])
+        correlation_matrix.loc[col1, col2] = corr
+        p_value_matrix.loc[col1, col2] = p_value
+
+significant_correlation = correlation_matrix.where(p_value_matrix < .05, other=0)
+
+
+anova_results = pd.DataFrame(columns=['P-Value'])  # To store results
+
+for parameter in parameters:
+    # Group the feature by the 'Quality' categories
+    groups = [wq[wq['Quality'] == category][parameter] for category in wq['Quality'].unique()]
+
+    # Perform one-way ANOVA
+    f_stat, p_value = stats.f_oneway(*groups)
+
+    # anova_results.loc[parameter] = p_value
+    st.write(p_value)
+
+# st.dataframe(anova_results)
+
+
+st.subheader("Parameters with Significant Correlation Heatmap")
 
 fig, ax = plt.subplots(figsize=(12, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='YlGnBu', ax=ax)
-
-st.pyplot(fig)
-
-
-feature_correlations = wq[parameters].corrwith(wq['Water Quality'])
-
-st.header('Feature Correlation with Water Quality')
-
-fig, ax = plt.subplots(figsize=(10, 8))
-feature_correlations.sort_values(ascending=False).plot(kind='bar', ax=ax)
-ax.set_title("Correlation of Features with Water Quality")
-ax.set_ylabel("Correlation Coefficient")
-ax.set_xlabel("Features")
+sns.heatmap(significant_correlation, annot=True, ax=ax)
 
 st.pyplot(fig)
