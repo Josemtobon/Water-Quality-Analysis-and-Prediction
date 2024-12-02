5db6f3f401ff88dd4222310caa9675cd9fd395de
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-12-02 00:12:53 -0500

    Performed all the tests

--- pages/Parameter Correlation Analysis.py
+++ pages/Parameter Correlation Analysis.py
@@ -13,42 +13,44 @@ parameters = wq.columns[:-2]
 
 # Normality test
 # DataFrame to store Shapiro-Wilk results
-shapiro_results = pd.DataFrame(columns=['P-Value', 'Normally Distributed'])
+shapiro_results = pd.DataFrame(columns=['Parameter', 'Group', 'P-Value', 'Normally Distributed'])
 
 for column in parameters:
-    stat, p_value = stats.shapiro(wq[column])
-
-    shapiro_results.loc[column] = [str(p_value), int(p_value < .05)]
+    for name, group in wq.groupby('Quality'):
+        _, p_value = stats.shapiro(group[column])
+        shapiro_results.loc[len(shapiro_results)] = [column, name, str(p_value), str(p_value < .05)]
 
 # To show table results on page
 st.subheader("Shapiro-Wilk Results for Each Parameter")
 st.dataframe(shapiro_results)
 
 
-anova_results = pd.DataFrame(columns=['P-Value', 'Reject Null Hypothesis'])
+# Homoscedasticity test
+# DataFrame to store Bartlett results
+bartlett_results = pd.DataFrame(columns=['Parameter', 'P-Value', 'Homoscedasticity'])
+
+for column in parameters:
+    groups = [group[column].values for name, group in wq.groupby('Water Quality')]
+    _, p_value = stats.bartlett(*groups)
+    bartlett_results.loc[len(bartlett_results)] = [column, str(p_value), str(p_value < .05)]
+
+# To show table results on page
+st.subheader("Bartlett Results for Each Parameter")
+st.dataframe(bartlett_results)
+
+
+anova_results = pd.DataFrame(columns=['Parameter', 'P-Value', 'significant Difference'])
 tukey_results = pd.DataFrame(columns=[])
 
 for parameter in parameters:
     groups = [group[parameter].values for name, group in wq.groupby('Water Quality')]
 
     _, p_value = stats.f_oneway(*groups)
-    if p_value < .05:
-        sd = 'Yes'
-    else:
-        sd = 'No'
 
-    anova_results.loc[parameter] = str(p_value), sd
+    anova_results.loc[len(anova_results)] = [parameter, str(p_value), str(p_value < .05)]
 
     tukey = pairwise_tukeyhsd(endog=wq[parameter],
                                groups=wq['Quality'],
                                alpha=0.05)
 
 st.dataframe(anova_results)
-
-
-st.subheader("Parameters with Significant Correlation Heatmap")
-
-fig, ax = plt.subplots(figsize=(12, 8))
-sns.heatmap(significant_correlation, annot=True, ax=ax)
-
-st.pyplot(fig)
