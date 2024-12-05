b8bd75e339d0816dca98b1fe2290782fe9b3b7c7
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-12-04 23:39:42 -0500

    Interval table

--- pages/Distribution of Parameters.py
+++ pages/Distribution of Parameters.py
@@ -8,7 +8,7 @@ import seaborn as sns
 
 wq = pd.read_csv("WQD.tsv", sep="\t")
 wq = wq.rename(columns={wq.columns[4]: 'CO₂', wq.columns[5]: 'pH'})
-wq['Quality'] = wq['Water Quality'].map({0: 'Execellent', 1: 'Good', 2: 'Poor'})
+wq['Quality'] = wq['Water Quality'].map({0: 'Excellent', 1: 'Good', 2: 'Poor'})
 
 parameters = wq.columns[:-2]
 
@@ -20,7 +20,7 @@ parameter = st.selectbox(
     parameters
 )
 
-qualities = st.pills("Water Quality", ["Execellent", "Good", "Poor"], selection_mode="multi", default=["Execellent", "Good", "Poor"])
+qualities = st.pills("Water Quality", ["Excellent", "Good", "Poor"], selection_mode="multi", default=["Excellent", "Good", "Poor"])
 filtered_wq = wq[wq['Quality'].isin(qualities)]
 
 fig, ax = plt.subplots(figsize=(10, 8))
@@ -34,18 +34,45 @@ if qualities:
     st.pyplot(fig)
 
 
-st.header('Parameter Resume')
+st.header(f'{parameter} Resume')
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.subheader('Excellent')
-    st.write(wq[parameter][wq['Quality'] == 'Execellent'].describe())
+    st.table(wq[parameter][wq['Quality'] == 'Excellent'].describe())
 
 with col2:
     st.subheader('Good')
-    st.write(wq[parameter][wq['Quality'] == 'Good'].describe())
+    st.table(wq[parameter][wq['Quality'] == 'Good'].describe())
 
 with col3:
     st.subheader('Poor')
-    st.write(wq[parameter][wq['Quality'] == 'Poor'].describe())
+    st.table(wq[parameter][wq['Quality'] == 'Poor'].describe())
+
+
+st.header(f'{parameter} Resume')
+
+st.write("For excellent water quality, the data shows well-defined ranges with a lower standard deviation compared to other water quality categories. Good quality is distributed near the edges of the range limits for excellent quality, while poor quality exhibits the highest standard deviation and includes atypical values.")
+
+
+rows = []
+
+for param in parameters:
+    summary_row = {'Parameter': param}
+
+    for quality, group in wq.groupby('Quality'):
+        min_val = group[param].min()
+        max_val = group[param].max()
+        interval = f"{min_val} - {max_val}"
+
+        summary_row[f"{quality} Quality"] = interval
+
+    optimal_value = wq.loc[wq['Quality'] == 'Excellent', param].mean()
+    summary_row['Optimal Value'] = optimal_value
+
+    rows.append(summary_row)
+
+summary_table = pd.DataFrame(rows)
+
+st.table(summary_table.set_index('Parameter'))
