259809acf6db47b8a499aef3df7e20e049510add
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-12-07 17:53:54 -0500

    Boxplots removed

--- pages/Classifiers Analysis.py
+++ pages/Classifiers Analysis.py
@@ -4,12 +4,4 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 results = pd.read_csv('performance_results.tsv', sep='\t')
-st.dataframe(results.head())
-
-fig, axs = plt.subplots(3, 1, figsize=(8, 14))
-sns.boxplot(results, x='Precision', y='Model', hue='Model', ax=axs[0])
-sns.boxplot(results, x='Recall', y='Model', hue='Model', ax=axs[1])
-sns.boxplot(results, x='F1', y='Model', hue='Model', ax=axs[2])
-plt.tight_layout()
-
-st.pyplot(fig)
+st.table(results.head())
