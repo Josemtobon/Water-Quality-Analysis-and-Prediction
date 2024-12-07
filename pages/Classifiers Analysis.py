abcbf2293d975d5b93fbffa9b2724cd0e00ca29c
Jose M. Tobón <josem.tobon@udea.edu.co>
2024-12-07 18:15:59 -0500

    Containers

--- pages/Classifiers Analysis.py
+++ pages/Classifiers Analysis.py
@@ -6,6 +6,14 @@ import seaborn as sns
 results = pd.read_csv('performance_results.tsv', sep='\t')
 st.table(results.head())
 
-st.image("confusion_matrix_K-Nearest Neighbors.png")
-st.image("confusion_matrix_Random Forest.png")
-st.image("confusion_matrix_SVM.png")
+col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
+
+with col1:
+    st.image("confusion_matrix_K-Nearest Neighbors.png")
+
+with col2:
+    st.image("confusion_matrix_Random Forest.png")
+
+with col3:
+    st.image("confusion_matrix_SVM.png")
